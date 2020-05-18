4cbef0839f0c47b9d1e973add36046b3f8197c0a
NAre5 <sherlord5@gmail.com>
2020-05-18 22:12:12 +0300

    for class

--- codes/solution/middleware_task.js
+++ codes/solution/middleware_task.js
@@ -4,32 +4,44 @@ const app = express();
 app.use(express.json()); // (?) parse application/json
 app.use(logger("dev")); // (?) logger
 
-const message = "default";
-
+//#region run app.use with a middleware that always execute - console.log("welcome to my server")
 app.use((req, res, next) => {
   console.log("welcome to my server"); // (1) Execute any code.
   next(); // (4) Call the next middleware function in the stack.
 });
+//#endregion
 
+//#region run app.use under the path "/numbers" with 2 middlewares that the first exexute - console.log(1), and the second - console.log(2)
 app.use(
   "/numbers",
   (req, res, next) => {
-    console.log("一"); // (1) Execute any code.
+    console.log("1"); // (1) Execute any code.
     next(); // (4) Call the next middleware function in the stack.
   },
   (req, res, next) => {
-    console.log("二"); // (1) Execute any code.
+    console.log("2"); // (1) Execute any code.
     next(); // (4) Call the next middleware function in the stack.
   }
 );
+//#endregion
 
+//#region run app.use with a middleware that always add the request body message parameter equal to "message"
 app.use((req, res, next) => {
-  if (!req.body.message) req.body.message = message; // (2) Make changes to the request and the response objects.
+  req.body.message = "message"; // (2) Make changes to the request and the response objects.
   next(); // (4) Call the next middleware function in the stack.
 });
+//#endregion
 
+//#region run app.use with a middleware that respond to the client with "last resort"
 app.use((req, res, next) => {
   res.send("last resort"); // (3) End the request-response cycle
+});
+//#endregion
+
+// error middleware
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).send({ message: err.message, success: false });
 });
 
 const server = app.listen(3000, () => {
