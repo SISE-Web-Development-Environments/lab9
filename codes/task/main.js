a8494cf3314186a35541e5897a6f785f3f0fcf64
NAre5 <sherlord5@gmail.com>
2020-05-24 13:53:35 +0300

    for class

--- codes/task/main.js
+++ codes/task/main.js
@@ -25,7 +25,55 @@ var port = process.env.PORT || "3000";
 //#endregion
 const user = require("./routes/user");
 const profile = require("./routes/profile");
-const recipes = require("./routes/recipes");
+
+//#region this is belong to recipes module
+const axios = require("axios");
+const api_domain = "https://api.spoonacular.com/recipes";
+
+app.get("/recipes/Information", async (req, res, next) => {
+  try {
+    const recipe = await getRecipeInfo(req.query.recipe_id);
+    res.send({ data: recipe.data });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/recipes/search", async (req, res, next) => {
+  try {
+    const { query, cuisine, diet, intolerances, number } = req.query;
+    const search_response = await axios.get(`${api_domain}/search`, {
+      params: {
+        query: query,
+        cuisine: cuisine,
+        diet: diet,
+        intolerances: intolerances,
+        number: number,
+        instructionsRequired: true,
+        apiKey: process.env.spooncular_apiKey
+      }
+    });
+    let recipes = await Promise.all(
+      search_response.data.results.map((recipe_raw) =>
+        getRecipeInfo(recipe_raw.id)
+      )
+    );
+    recipes = recipes.map((recipe) => recipe.data);
+    res.send({ data: recipes });
+  } catch (error) {
+    next(error);
+  }
+});
+
+function getRecipeInfo(id) {
+  return axios.get(`${api_domain}/${id}/information`, {
+    params: {
+      includeNutrition: false,
+      apiKey: process.env.spooncular_apiKey
+    }
+  });
+}
+//#endregion
 
 //#region cookie middleware
 app.use(function (req, res, next) {
