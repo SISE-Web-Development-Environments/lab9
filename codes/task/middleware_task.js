dd3d8414b7545763956433827e2b8df4582cc9e0
NAre5 <sherlord5@gmail.com>
2020-05-24 17:47:59 +0300

    for class

--- codes/task/middleware_task.js
+++ codes/task/middleware_task.js
@@ -12,29 +12,20 @@ app.use((req, res, next) => {
 //#endregion
 
 //#region run app.use with a middleware that always add the request body message parameter equal to "message"
-app.use((req, res, next) => {
-  req.body.message = "message";
-  // req.body[message] = "message";
-  next();
-});
 //#endregion
 
 //#region run app.use with a middleware that respond to the client with message "last resort" and req.body
 //#endregion
 
-app.get("/", async (req, res, next) => {
-  // res.send(req.body);
-  // next();
-  next(new Error("error"));
+app.get("/", (req, res, next) => {
+  res.send(req.body);
 });
 
 // error middleware
-app.use((err, req, res, next) => {
-  // console.error(err);
-  res
-    .status(500)
-    .send({ message: err.message, success: false, body: req.body });
-});
+// app.use(function (err, req, res, next) {
+//   console.error(err);
+//   res.status(500).send({ message: err.message, success: false });
+// });
 
 const server = app.listen(3001, () => {
   console.log("party started at port", 3001);
